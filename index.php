730b9278d0d4b2541dc3fc8b4bd103f1e4864ea7
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-10-31 13:14:01 -0400

    created login form
    Create the basic login form for users to log into the system. Still need to implement that actual form action, however.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'includes/bootstrap-scripts.php';?>
+    <link rel="stylesheet" href="styles/styles.css">
+    <meta charset="UTF-8">
+    <title>Log In</title>
+</head>
+
+<body>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="shadow p-3 mb-5 bg-white rounded">
+            <h3 class="text-center">Log In</h3>
+            <form>
+                <div class="form-group">
+                    <label for="email">Email Address</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Email Address"
+                        required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password"
+                        required>
+                </div>
+                <button type="submit" class="btn btn-primary w-100 text-center">Submit</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
