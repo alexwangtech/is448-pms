d20be7f846dd45edde80b77d949702320b264c0f
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-10-31 15:48:40 -0400

    implement sticky top navbar
    Changed the padding in the main content <div> and added the "sticky-top" class to the navbar element to create a navbar that sticks to the top of the viewport at all times.

--- components/navbar.php
+++ components/navbar.php
@@ -1,7 +1,7 @@
 <!-- Navbar Component -->
 <!-- To use this, include it in your #mainContent div -->
 
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-light bg-light sticky-top">
     <?php include 'components/sidebar-button.php';?>
     <button type="button" class="btn btn-outline-secondary">Log Out</button>
 </nav>
