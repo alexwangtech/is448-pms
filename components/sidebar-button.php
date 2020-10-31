c04f63b7e3fd312cd59acbceb9d172eccc4bc926
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-10-31 10:14:07 -0400

    remove sidebar button text
    Removed the "Toggle Sidebar" text from the sidebar button - now it's just the three horizontal stripes.

--- components/sidebar-button.php
+++ components/sidebar-button.php
@@ -2,4 +2,4 @@
 <!-- Refer to sidebar.php -->
 <!-- This component is included in navbar.php -->
 
-<button class="openbtn" onclick="toggleNav()"> &#9776; Toggle Sidebar</button>
+<button class="openbtn" onclick="toggleNav()">&#9776;</button>
