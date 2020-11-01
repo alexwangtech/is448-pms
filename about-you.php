00ffb4d32b2fac40647ec2d5f98e00ef39a466c3
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-10-31 23:41:13 -0400

    add sidebar + navbar to about you
    Added the sidebar and the navbar to the "About You" page.

--- about-you.php
+++ about-you.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php include 'includes/bootstrap-scripts.php';?>
-        <link rel="stylesheet" href="styles/styles.css">
-        <meta charset="UTF-8">
-        <title>About You</title>
-    </head>
-    <body>
+
+<head>
+    <?php include 'includes/bootstrap-scripts.php';?>
+    <link rel="stylesheet" href="styles/styles.css">
+    <meta charset="UTF-8">
+    <title>About You</title>
+</head>
+
+<body>
+    <?php include 'components/sidebar.php';?>
+    <div id="mainContent">
+        <?php include 'components/navbar.php';?>
         <h1>About You Page</h1>
-    </body>
+    </div>
+</body>
+
 </htmL>
