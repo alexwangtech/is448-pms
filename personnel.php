ec702644a2e8987208f223de8ed9a821c8923446
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-11-01 02:47:20 -0500

    refactor structure of form fields
    This isn't much of a step forward, but it's a slight refactor in the <div> structure of the search fields in the "Personnel" page. This might possibly make it possible to use the "Enter" key to perform searches in the future.

--- personnel.php
+++ personnel.php
@@ -20,27 +20,37 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-11">
-                    <div class="form-row">
-                        <div class="form-group col">
-                            <input id="firstNameField" type="text" name="firstName" placeholder="First Name"
-                                class="form-control">
+                <div class="col">
+                    <form>
+                        <div class="container-fluid p-0">
+                            <div class="row">
+                                <div class="col-11">
+                                    <div class="form-row">
+                                        <div class="form-group col">
+                                            <input id="firstNameField" type="text" name="firstName"
+                                                placeholder="First Name" class="form-control">
+                                        </div>
+                                        <div class="form-group col">
+                                            <input id="lastNameField" type="text" name="lastName"
+                                                placeholder="Last Name" class="form-control">
+                                        </div>
+                                        <div class="form-group col">
+                                            <input id="departmentField" type="text" name="departmentName"
+                                                placeholder="Department Name" class="form-control">
+                                        </div>
+                                        <div class="form-group col">
+                                            <input id="emailField" type="text" name="email" placeholder="Email"
+                                                class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-1">
+                                    <button id="searchButton" type="button"
+                                        class="btn btn-outline-primary">Search</button>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group col">
-                            <input id="lastNameField" type="text" name="lastName" placeholder="Last Name"
-                                class="form-control">
-                        </div>
-                        <div class="form-group col">
-                            <input id="departmentField" type="text" name="departmentName" placeholder="Department Name"
-                                class="form-control">
-                        </div>
-                        <div class="form-group col">
-                            <input id="emailField" type="text" name="email" placeholder="Email" class="form-control">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-1">
-                    <button id="searchButton" type="button" class="btn btn-outline-primary">Search</buston>
+                    </form>
                 </div>
             </div>
             <div class="row">
