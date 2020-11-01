36233bc7b5b50a2c114ba4f11b85398b313bb809
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-11-01 04:05:22 -0500

    fixed aria typo
    Accidentally put "aira' instead of "aria"

--- personnel.php
+++ personnel.php
@@ -89,7 +89,7 @@
                 <div class="modal-header">
                     <h5 class="modal-title">New Personnel</h5>
                     <button type="button" class="close" data-dismiss="modal">
-                        <span aira-hidden="true">&times;</span>
+                        <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
