e9994536423a31abcf92168ce9c3d64c92be42f7
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-10-31 23:39:47 -0400

    implement link to about you page
    Implemented the linking to the "About You" page in the sidebar navigation.

---
+++ about-you.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include 'includes/bootstrap-scripts.php';?>
+        <link rel="stylesheet" href="styles/styles.css">
+        <meta charset="UTF-8">
+        <title>About You</title>
+    </head>
+    <body>
+        <h1>About You Page</h1>
+    </body>
+</htmL>
