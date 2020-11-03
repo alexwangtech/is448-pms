2790e4672a740a342240d0b0a624af98df3d2206
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-11-03 03:18:23 -0500

    change tasks page title
    Changed the title of the tasks page to "My Tasks"

--- tasks.php
+++ tasks.php
@@ -12,7 +12,7 @@
     <?php include 'components/sidebar.php';?>
     <div id="mainContent">
         <?php include 'components/navbar.php';?>
-        <h1>Tasks Page</h1>
+        <h1>My Tasks</h1>
 
         <div id="taskList"></div>
         <script src="js/tasklist.js"></script>
